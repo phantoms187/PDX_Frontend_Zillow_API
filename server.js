08a7b6ac2b6db8f50954efd6d98fa9c097cd1194
trip <dwd2@pdx.edu>
2020-02-23 21:32:22 -0800

    Added basic server with weather and geo location api

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require("path");
+const request = require('request');
+var moment = require('moment');
+
+
+const port = process.env.PORT || 4000;
+const darkSkyAPI = process.env.darkSky; //For weather info
+
+var NodeGeocoder = require('node-geocoder');
+var options = {
+  provider: 'opencage',
+  apiKey: process.env.openCageAPI
+};
+var geocoder = NodeGeocoder(options);
+function getCoordinates(place) {
+    return new Promise((resolve, reject) => {
+        geocoder.geocode(place, function(err,res) {
+            if(err)
+                reject(err);
+            else
+                resolve(res);
+            });
+    });
+}
+// Routes
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client/pdx-zillow-api/build/')));
+
+app.post('/weather', (req, res) => {
+
+    let lat = '';
+    let long = '';
+    let place = req.body.street + " " + req.body.city + ", " + req.body.state + " " + req.body.zip;
+    let time = moment(req.body.date + " " + req.body.time, 'dddd, MMMM Do YYYY h:mm A').unix();
+
+    async () => {
+      const geoCoordinates = await getCoordinates(place);
+        lat = geoCoordinates[0].latitude;
+        long = geoCoordinates[0].longitude;
+        request('https://api.darksky.net/forecast/' + darkSkyAPI + '/' + lat +',' + long + ',' + time, function (error, response, body) {
+            const newBody = JSON.parse(body);
+            res.json(newBody);
+        });
+        lat = '';
+        long = '';
+    };
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, './client/pdx-zillow-api/build/index.html'));
+});
+
+app.listen(port, () =>{
+  console.log('Server is running on Port:', port);
+});
