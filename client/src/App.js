d9ab33acc25d1c1ec76a4dc4d68a1068f4ea4b30
Mi Yon Kim <youn0125@gmail.com>
2020-03-03 10:39:45 -0800

    Add route path to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route} from "react-router-dom";
+import AboutWS from "./routes/AboutWS";
+import Home from "./routes/Home";
+// import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hi Mi Yon!
-        </p>
-      
-      </header>
-    </div>
+    <HashRouter>
+      {/* <Navigation /> */}
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/walkscore" exact={true} component={AboutWS} />
+    </HashRouter>
   );
 }
 
