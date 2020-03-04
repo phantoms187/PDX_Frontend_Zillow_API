a127ca2b244edaefb1199229b8d80dc7c0238762
Mi Yon Kim <youn0125@gmail.com>
2020-03-04 00:57:43 -0800

    Get data using walkscore API

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const cors = require('cors');
 const path = require("path");
 const request = require('request');
 var moment = require('moment');
-var Zillow = require('node-zillow');
+const dotenv = require('dotenv');
+const axios = require('axios');
+const Zillow = require('node-zillow');
 
-
+dotenv.config();
 const port = process.env.PORT || 4000;
 const darkSkyAPI = process.env.darkSkyAPI; //For weather info
 var zillowAPI = new Zillow(process.env.zillowAPI);
+const walkScoreAPI = process.env.walkScoreAPI; 
 
 var NodeGeocoder = require('node-geocoder');
 var options = {
@@ -54,7 +57,42 @@ app.post('/weather', (req, res) => {
     };
 });
 
-app.get("/*", (req, res) => {
+app.get('/walkscore', (req, res) => {
+  //url shown in walk score api example page.
+  let url = `http://api.walkscore.com/score?format=json&address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=47.6085&lon=-122.3295&transit=1&bike=1&wsapikey=${walkScoreAPI}`
+  axios.get(url)
+    .then( (response) => {
+      res.send(response.data);  
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+      
+});
+
+
+// app.post('/walkscore', (req, res) => {
+
+//   let lat = '';
+//   let lon = '';
+//   let place = '';
+
+//   async () => {
+//     const geoCoordinates = await getCoordinates(place);
+//       lat = geoCoordinates[0].latitude;
+//       lon = geoCoordinates[0].longitude;
+//       console.log("lat: " +lat);
+//       console.log("long: " +long);
+//       request(`http://api.walkscore.com/score?format=json&address=${place}&lat=${lat}&lon=${lon}&wsapikey=${walkScoreAPI}`,  (error, response, body) => {
+//          const newBody = JSON.parse(body);
+//          res.json(newBody);
+//       });
+//       lat = '';
+//       long = '';
+//   };
+// });
+
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
