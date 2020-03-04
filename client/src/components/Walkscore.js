163c57dcab77559f313fe91e8cc9340685d0f556
Mi Yon Kim <youn0125@gmail.com>
2020-03-04 13:34:08 -0800

    style walkscore component

--- client/src/components/Walkscore.js
+++ client/src/components/Walkscore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Walkscore.css';
 
 
 function addDescription(score) {
@@ -7,17 +8,33 @@ function addDescription(score) {
     return "Almost all errands require a car.";
   else if ( Number(score) <= 49)
     return "Most errands require a car.";
+  else if ( Number(score) <= 69)
+    return "Some errands can be accomplished on foot.";
+  else if ( Number(score) <= 89)
+    return "Most errands can be accomplished on foot.";
+  else if ( Number(score) <= 100)
+    return "Daily errands do not require a car.";
   else
-    return "not yet";
+    return "Not yet decided.";
 }
 function Walkscore({score, description}){
   let description_sec = addDescription(score);
   return ( 
-    <div className="walkscore_data">
-      <h1 className="walkscore_score">{score}</h1>
-      <h1 className="walkscore_desc">{description}</h1>
-      <h1 className="walkscore_desc_sec">{description_sec}</h1>
-    </div>);
+    <section className="walkscore_data">
+      <div className="walkscore_score box">
+        <h5>walkscore</h5>
+        <h1>44</h1>
+      </div>
+      <div className="walkscore_desc_all box">
+        <div className="walkscore_desc box">
+          <h3>{description}</h3>
+        </div>
+        <div className="walkscore_desc_sec box">
+          {description_sec}
+        </div>
+      </div>
+    </section>);
 }
 
 export default Walkscore;
+
